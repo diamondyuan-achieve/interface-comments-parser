ae8f983f2bb775c323f12d90e14bbfab88ad75c3
DiamondYuan <admin@diamondyuan.com>
2019-03-21 11:14:50 +0800

    feat: redesign api

--- src/interface.ts
+++ src/interface.ts
@@ -1,3 +1,4 @@
+import { MethodInfo } from './interface';
 export interface IField {
   /**
    * @language en-US
@@ -37,4 +38,89 @@ export interface IFieldMeta {
   i18n: {
     [language: string]: IMeta;
   };
+}
+
+export interface FieldInfo {
+  /**
+   * 是否过期
+   */
+  deprecated: boolean;
+  /**
+   * 字段名称
+   */
+  name: string;
+  /**
+   * 是否必填
+   */
+  required: boolean;
+  /**
+   * 类型
+   */
+  type: string;
+  /**
+   * 第一次出现的版本号
+   */
+  since?: string;
+  /**
+   * 默认类型
+   */
+  default?: string;
+  /**
+   * 概要
+   */
+  summary?: string;
+  /**
+   * 详细说明
+   */
+  remarks?: string;
+  /**
+   * 引用
+   */
+  see?: string;
+}
+
+export interface MethodInfo {
+  /**
+   * 是否过期
+   */
+  deprecated: boolean;
+  /**
+   * 字段名称
+   */
+  name: string;
+  /**
+   * 是否必填
+   */
+  required: boolean;
+  /**
+   * 类型
+   */
+  type: string;
+  /**
+   * 第一次出现的版本号
+   */
+  since?: string;
+  /**
+   * 概要
+   */
+  summary?: string;
+  /**
+   * 详细说明
+   */
+  remarks?: string;
+  /**
+   * 引用
+   */
+  see?: string;
+  /**
+   * 参数列表
+   */
+  param: {
+    name: string;
+    description: string;
+  }[];
+  /**
+   * 返回结果
+   */
+  return: string;
 }
