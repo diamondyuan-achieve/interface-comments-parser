582d83c08310ad71c1c0b44ae5b68c4d91d7e18d
DiamondYuan <admin@diamondyuan.com>
2019-03-21 11:34:54 +0800

    wip: remove all code

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,75 +1,14 @@
-import assert = require('power-assert');
 import * as path from 'path';
-import { parse, getFieldMeta } from '../src/index';
-import { iTestInterfaceFieldMeta } from './fixtures/fixtures';
-import {
-  localeProviderPropsFieldMeta,
-  localeProviderPropsBaseMeta,
-  localeProviderPropsChineseMeta
-} from './fixtures/module';
+import * as fs from 'fs';
+import { parse } from '../src/index';
+import assert = require('power-assert');
 
-const iTestInterfaceFixtures = path.resolve(
-  __dirname,
-  './fixtures/fixtures.tsx'
-);
+const classFixtures = path.resolve(__dirname, './fixtures/fixtures.tsx');
 
-const classFixtures = path.resolve(__dirname, './fixtures/fixtures.d.ts');
-
-const moduleInterface = path.resolve(__dirname, './fixtures/module.tsx');
+const code = fs.readFileSync(classFixtures).toString();
 
 describe('test index.ts', () => {
-  describe('test parse function', () => {
-    it('should return meta correct', () => {
-      assert.deepEqual(
-        parse(iTestInterfaceFixtures, 'ITestInterface'),
-        iTestInterfaceFieldMeta
-      );
-    });
-    it('should return null when interface is not exist', () => {
-      assert.equal(parse(iTestInterfaceFixtures, 'NotExist'), null);
-    });
-    it('should support module', () => {
-      assert.deepEqual(
-        parse(moduleInterface, 'LocaleProviderProps'),
-        localeProviderPropsFieldMeta
-      );
-    });
-    it('support d.ts', () => {
-      assert.deepEqual(parse(classFixtures, 'ElAlert'), [
-        {
-          optional: 'false',
-          name: 'data',
-          types: 'string',
-          meta: { base: { description: 'test' }, i18n: {} }
-        },
-        { optional: 'false', name: 'type', types: 'AlertType' },
-        { optional: 'true', name: 'description', types: '' }
-      ]);
-    });
-  });
-
-  describe('test getFieldMeta', () => {
-    const fields = parse(moduleInterface, 'LocaleProviderProps');
-
-    it('should return baseMeta when language is empty ', () => {
-      assert.deepEqual(
-        fields.map(o => getFieldMeta(o)),
-        localeProviderPropsBaseMeta
-      );
-      assert.deepEqual(
-        fields.map(o => getFieldMeta(o, null)),
-        localeProviderPropsBaseMeta
-      );
-      assert.deepEqual(
-        fields.map(o => getFieldMeta(o, 'error')),
-        localeProviderPropsBaseMeta
-      );
-    });
-    it('should return zh-CN meta when language is zh-CN', () => {
-      assert.deepEqual(
-        fields.map(o => getFieldMeta(o, 'zh-CN')),
-        localeProviderPropsChineseMeta
-      );
-    });
+  it('should return null when pass empty interface name', () => {
+    assert.equal(parse(code, ''), null);
   });
 });
